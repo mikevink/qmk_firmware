17c650df71abfb94357de2893b00913d3ad74b0a
Mike Vink <code@vink.tech>
2021-01-23 00:31:06 +0000

    define keymap using own structs

--- keyboards/moonlander/keymaps/mikevink/keymap.c
+++ keyboards/moonlander/keymaps/mikevink/keymap.c
@@ -23,6 +23,155 @@ enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
 
+// todo: caps lock indicator maybe?
+
+// colours to use in the keymap
+//                  H    S    V
+#define V_COL_DED {  0,   0,   0}
+#define V_COL_SMB {  0,   0, 255}
+#define V_COL_SPC {127, 255, 255}
+#define V_COL_MOD { 21, 255, 255}
+#define V_COL_NAV {148, 255, 255}
+#define V_COL_MED { 85, 255, 255}
+#define V_COL_RGB { 35, 255, 255}
+#define V_COL_FN  {  0, 183, 238}
+#define V_COL_KP  {134, 255, 213}
+#define V_COL_RST {  0, 255, 255}
+#define V_COL_ORX { 64, 255, 255}
+#define V_COL_GUI V_COL_ORX
+const HSV V_COL_LYR[] = {
+    [0] =         {  0,   0, 255},
+    [1] =         {169, 255, 255},
+    [2] =         { 85, 255, 255},
+    [3] =         {  0, 255, 255},
+};
+
+// helpers for key definition
+typedef struct PACKED {
+    uint16_t key;
+    HSV hsv; // color.h, uint8_t {h, s, v}
+} V_KEY_DEF;
+#define V_DED       {    KC_NO,        V_COL_DED}
+#define V_SMB(key)  {      key,        V_COL_SMB}
+#define V_SPC(key)  {      key,        V_COL_SPC}
+#define V_MOD(key)  {      key,        V_COL_MOD}
+#define V_NAV(key)  {      key,        V_COL_NAV}
+#define V_MED(key)  {      key,        V_COL_MED}
+#define V_RGB(key)  {      key,        V_COL_RGB}
+#define V_FN(key)   {      key,         V_COL_FN}
+#define V_KP(key)   {      key,         V_COL_KP}
+#define V_RST(key)  {      key,        V_COL_RST}
+#define V_ORX(key)  {      key,        V_COL_ORX}
+#define V_GUI(key)  {      key,        V_COL_GUI}
+#define V_TO(layer) {TO(layer), V_COL_LYR[layer]}
+
+// keymaps go here, arranged in the RGB setup
+// that means first 36 entries are the left side of the keyboard, top to bottom, left to right
+//        the last 36 entries are the right side of the keyboard, top to bottom, right to left
+const V_KEY_DEF PROGMEM v_keymaps[][DRIVER_LED_TOTAL] = {
+    [0] = {
+        // left half, top to bottom, left to right
+        V_SMB(DP_DLR),          V_SPC(KC_TAB),              V_MOD(KC_CAPSLOCK),         V_MOD(KC_LSHIFT),   V_DED,
+        V_SMB(DP_AMPR),         V_SMB(DP_SCLN),             V_SMB(DP_A),                V_SMB(DP_QUOT),     V_MOD(KC_LCTRL),
+        V_SMB(DP_LBRC),         V_SMB(DP_COMM),             V_SMB(DP_O),                V_SMB(DP_Q),        V_GUI(KC_ESC),
+        V_SMB(DP_LCBR),         V_SMB(DP_DOT),              V_SMB(DP_E),                V_SMB(DP_J),        V_MOD(KC_RALT),
+        V_SMB(DP_RCBR),         V_SMB(DP_P),                V_SMB(DP_U),                V_SMB(DP_K),        V_MOD(KC_LALT),
+        V_SMB(DP_LPRN),         V_SMB(DP_Y),                V_SMB(DP_I),                V_SMB(DP_X),
+        V_SMB(DP_EQL),          V_NAV(KC_HOME),             V_NAV(KC_END),
+        // left flap
+        V_SPC(KC_SPACE),        V_SPC(KC_INSERT),           V_TO(1),
+        V_SPC(KC_BSPACE),
+        // right half, top to bottom, right to left
+        V_SMB(DP_HASH),         V_SMB(DP_AT),               V_SMB(DP_MINS),             V_SMB(DP_BSLS),     V_SPC(KC_ENTER),
+        V_SMB(DP_EXLM),         V_SMB(DP_L),                V_SMB(DP_S),                V_SMB(DP_Z),        V_NAV(KC_RIGHT),
+        V_SMB(DP_RBRC),         V_SMB(DP_R),                V_SMB(DP_N),                V_SMB(DP_V),        V_NAV(KC_DOWN),
+        V_SMB(DP_PLUS),         V_SMB(DP_C),                V_SMB(DP_T),                V_SMB(DP_W),        V_NAV(KC_UP),
+        V_SMB(DP_RPRN),         V_SMB(DP_G),                V_SMB(DP_H),                V_SMB(DP_M),        V_NAV(KC_LEFT),
+        V_SMB(DP_ASTR),         V_SMB(DP_F),                V_SMB(DP_D),                V_SMB(DP_B),
+        V_NAV(KC_PGUP),         V_NAV(KC_PGDOWN),           V_SMB(DP_SLSH),
+        // right flap
+        V_TO(2),                V_SPC(KC_DELETE),           V_SPC(KC_ENTER),
+        V_GUI(KC_LGUI),
+    },
+    [1] = {
+        // left half, top to bottom, left to right
+        V_DED,                  V_DED,                      V_DED,                      V_MOD(KC_LSHIFT),   V_DED,
+        V_DED,                  V_FN(KC_F1),                V_FN(KC_F5),                V_FN(KC_F9),        V_MOD(KC_LCTRL),
+        V_DED,                  V_FN(KC_F2),                V_FN(KC_F6),                V_FN(KC_F10),       V_DED,
+        V_DED,                  V_FN(KC_F3),                V_FN(KC_F7),                V_FN(KC_F11),       V_DED,
+        V_DED,                  V_FN(KC_F4),                V_FN(KC_F8),                V_FN(KC_F12),       V_MOD(KC_LALT),
+        V_DED,                  V_DED,                      V_DED,                      V_DED,
+        V_DED,                  V_DED,                      V_DED,
+        // left flap
+        V_DED,                  V_DED,                      V_TO(0),
+        V_DED,
+        // right half, top to bottom, right to left
+        V_DED,                  V_DED,                      V_DED,                      V_DED,              V_DED,
+        V_KP(KC_KP_MINUS),      V_KP(KC_KP_PLUS),           V_KP(KC_KP_PLUS),           V_KP(KC_KP_ENTER),  V_KP(KC_KP_ENTER),
+        V_KP(KC_KP_ASTERISK),   V_KP(KC_KP_9),              V_KP(KC_KP_6),              V_KP(KC_KP_3),      V_KP(KC_KP_DOT),
+        V_KP(KC_KP_SLASH),      V_KP(KC_KP_8),              V_KP(KC_KP_5),              V_KP(KC_KP_2),      V_KP(KC_KP_0),
+        V_DED,                  V_KP(KC_KP_7),              V_KP(KC_KP_4),              V_KP(KC_KP_1),      V_KP(KC_KP_0),
+        V_DED,                  V_DED,                      V_DED,                      V_DED,
+        V_DED,                  V_DED,                      V_DED,
+        // right flap
+        V_DED,                  V_DED,                      V_DED,
+        V_DED,
+    },
+    [2] = {
+        // left half, top to bottom, left to right
+        V_DED,                      V_DED,                  V_DED,                      V_MOD(KC_LSHIFT),   V_DED,
+        V_MED(KC_MEDIA_PREV_TRACK), V_DED,                  V_NAV(KC_MS_LEFT),          V_DED,              V_MOD(KC_LCTRL),
+        V_MED(KC_MEDIA_STOP),       V_NAV(KC_MS_UP),        V_NAV(KC_MS_DOWN),          V_DED,              V_DED,
+        V_MED(KC_MEDIA_PLAY_PAUSE), V_DED,                  V_NAV(KC_MS_RIGHT),         V_DED,              V_DED,
+        V_MED(KC_MEDIA_NEXT_TRACK), V_NAV(KC_MS_WH_UP),     V_NAV(KC_MS_WH_DOWN),       V_DED,              V_MOD(KC_LALT),
+        V_DED,                      V_DED,                  V_DED,                      V_DED,
+        V_MED(KC_AUDIO_MUTE),       V_MED(KC_AUDIO_VOL_UP), V_MED(KC_AUDIO_VOL_DOWN),
+        // left flap
+        V_NAV(KC_MS_BTN1),          V_NAV(KC_MS_BTN2),      V_DED,
+        V_NAV(KC_MS_BTN3),
+        // right half, top to bottom, right to left
+        V_RST(RESET),               V_DED,                  V_DED,                      V_RGB(RGB_VAI),     V_RGB(RGB_VAD),
+        V_DED,                      V_DED,                  V_DED,                      V_DED,              V_DED,
+        V_DED,                      V_DED,                  V_DED,                      V_DED,              V_DED,
+        V_RGB(RGB_HUD),             V_RGB(RGB_SLD),         V_RGB(RGB_TOG),             V_RGB(RGB_SPD),     V_RGB(RGB_SAD),
+        V_RGB(RGB_HUI),             V_RGB(RGB_MOD),         V_RGB(TOGGLE_LAYER_COLOR),  V_RGB(RGB_SPI),     V_RGB(RGB_SAI),
+        V_DED,                      V_DED,                  V_DED,                      V_DED,
+        V_ORX(WEBUSB_PAIR),         V_DED,                  V_DED,
+        // right flap
+        V_TO(0),                    V_TO(3),                V_DED,
+        V_DED,
+    },
+    [3] = {
+        // left half, top to bottom, left to right
+        V_GUI(KC_GESC),             V_DED,                  V_DED,                      V_DED,              V_DED,
+        V_SMB(DP_1),                V_SMB(DP_Q),            V_SMB(DP_A),                V_SMB(DP_Z),        V_MOD(KC_LCTRL),
+        V_SMB(DP_2),                V_SMB(DP_W),            V_SMB(DP_S),                V_SMB(DP_X),        V_DED,
+        V_SMB(DP_3),                V_SMB(DP_E),            V_SMB(DP_D),                V_SMB(DP_C),        V_DED,
+        V_SMB(DP_4),                V_SMB(DP_R),            V_SMB(DP_F),                V_SMB(DP_V),        V_MOD(KC_LALT),
+        V_SMB(DP_5),                V_SMB(DP_T),            V_SMB(DP_G),                V_SMB(DP_B),
+        V_SMB(DP_LBRC),             V_SMB(DP_RBRC),         V_FN(KC_F5),
+        // left flap
+        V_SPC(KC_SPACE),            V_SPC(KC_INSERT),       V_GUI(KC_ESC),
+        V_SPC(KC_BSPACE),
+        // right half, top to bottom, right to left
+        V_DED,                      V_DED,                  V_DED,                      V_DED,              V_SPC(KC_ENTER),
+        V_SMB(DP_0),                V_SMB(DP_P),            V_SMB(DP_SCLN),             V_SMB(DP_SLSH),     V_NAV(KC_RIGHT),
+        V_SMB(DP_9),                V_SMB(DP_O),            V_SMB(DP_L),                V_SMB(DP_DOT),      V_NAV(KC_DOWN),
+        V_SMB(DP_8),                V_SMB(DP_I),            V_SMB(DP_K),                V_SMB(DP_COMM),     V_NAV(KC_UP),
+        V_SMB(DP_7),                V_SMB(DP_U),            V_SMB(DP_J),                V_SMB(DP_M),        V_NAV(KC_LEFT),
+        V_SMB(DP_6),                V_SMB(DP_Y),            V_SMB(DP_H),                V_SMB(DP_N),
+        V_DED,                      V_DED,                  V_DED,
+        // right flap
+        V_TO(0),                    V_SPC(KC_DELETE),       V_SPC(KC_ENTER),
+        V_GUI(KC_LGUI),
+    },
+};
+
+
+
+// left to right, top to bottom
+// big red buttons are in line with the first to last row
+// last 6 keys are ltr as well
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     DP_DLR,         DP_AMPR,            DP_LBRC,            DP_LCBR,                DP_RCBR,                DP_LPRN,            DP_EQL,         KC_PGUP,        DP_ASTR,    DP_RPRN,            DP_PLUS,        DP_RBRC,        DP_EXLM,        DP_HASH,
@@ -46,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_MS_LEFT,             KC_MS_DOWN,     KC_MS_RIGHT,            KC_MS_WH_DOWN,          KC_NO,          KC_AUDIO_VOL_DOWN,  KC_NO,          KC_NO,      TOGGLE_LAYER_COLOR, RGB_TOG,        KC_NO,          KC_NO,          KC_NO,
     KC_LSHIFT,      KC_NO,                  KC_NO,          KC_NO,                  KC_NO,                  KC_NO,                                              KC_NO,      RGB_SPI,            RGB_SPD,        KC_NO,          KC_NO,          RGB_VAI,
     KC_NO,          KC_LCTRL,               KC_NO,          KC_NO,                  KC_LALT,                KC_MS_BTN3,                                         KC_NO,      RGB_SAI,            RGB_SAD,        KC_NO,          KC_NO,          RGB_VAD,
-                                                            KC_MS_BTN1,             KC_MS_BTN2,             KC_NO,                                              KC_NO,      TO(3),              KC_NO
+                                                            KC_MS_BTN1,             KC_MS_BTN2,             KC_NO,                                              TO(0),      TO(3),              KC_NO
   ),
   [3] = LAYOUT_moonlander(
     KC_GESC,        DP_1,                   DP_2,           DP_3,                   DP_4,                   DP_5,         DP_LBRC,            KC_NO,            DP_6,       DP_7,               DP_8,           DP_9,           DP_0,           KC_NO,
@@ -65,14 +214,31 @@ void keyboard_post_init_user(void) {
   rgb_matrix_enable();
 }
 
+// top to bottom, left to right
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
   [0] = {
-    {0,0,255},      {0,0,255},              {0,0,255},      {0,0,255},              {0,0,255},              {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},      {0,0,255},
-    {0,0,255},      {0,0,255},              {0,0,255},      {0,0,255},              {0,0,255},              {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},      {0,0,255},
-    {0,0,255},      {0,0,255},              {0,0,255},      {0,0,255},              {0,0,255},              {0,0,255},      {240,90,90},        {0,0,255},      {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},      {0,0,255},
-    {0,0,255},      {0,0,255},              {0,0,255},      {0,0,255},              {0,0,255},              {0,0,255},                                          {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},      {0,0,255},
-    {0,0,255},      {0,0,255},              {0,0,255},      {0,0,255},              {0,0,255},              {0,0,255},                                          {0,0,255},      {0,0,255},          {0,0,255},      {0,0,255},      {0,0,255},      {0,0,255},
-                                                            {0,0,255},              {0,0,255},              {0,0,255},                                          {0,0,255},      {240,90,90},        {0,0,255}
+    // left half, top to bottom, left to right
+    {0,0,255},    {32,255,234},    {32,255,234},    {32,255,234},    {0,0,0},
+    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},    {0,0,0},    {0,0,0},
+    // left flap
+    {0,0,0},    {0,0,0},    {169, 255,255},
+    {0,255,255},
+    // right half, top to bottom, right to left
+    {0,255,255},    {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,255},      {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},        {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},        {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},        {0,0,0},    {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},        {0,0,0},    {0,0,0},    {0,0,0},
+    {0,0,0},        {0,0,0},    {0,0,0},
+    // right flap
+    {0,0,0},        {0,0,0},    {0,0,0},
+    {0,0,0}
   },
   [1] = {
     {0,0,0},        {0,0,0},                {0,0,0},        {0,0,0},                {0,0,0},                {0,0,0},        {0,183,238},        {0,183,238},    {0,183,238},    {0,0,0},            {0,0,0},        {0,183,238},    {0,183,238},    {0,183,238},
